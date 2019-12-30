7dda21515f5489f986452c6c228fd99c3be559ff
Alessandro Mancini <dott.alessandro.mancini@gmail.com>
2019-12-30 14:15:01 +0100

    enabled home assistant auto discovery

--- README.md
+++ README.md
@@ -13,45 +13,52 @@ Create a file config.json in your project directory.
     "username": "YOUR_RISCO_EMAIL",
     "password": "YOUR_RISCO_PASSWORD",
     "pin": "YOUR_CENTRAL_PIN_CODE",
-    "languageId": "YOUR_LANGUAGE_ID", // example: en, it, de etc ...
-    "mqttURL": "mqtt://MQTT_HOST:MQTT_PORT",
-    "mqttUsername": "MQTT_USERNAME",
-    "mqttPassword": "MQTT_PASSWORD"
+    "language-id": "YOUR_LANGUAGE_ID", // example: en, it, de etc ...
+    "mqtt-url": "mqtt://MQTT_HOST:MQTT_PORT",
+    "mqtt-username": "MQTT_USERNAME",
+    "mqtt-password": "MQTT_PASSWORD",
+    "interval-polling": "RISCO_INTERVAL_POLLING", // default is 5000
+    "home-assistant-discovery-prefix" : "YOUR_HOME-ASSISTANT-DISCOVERY-PREFIX" // default is homeassistant
 }
 
 ```
 
 ## Subscribe Topics
 
-risco-mqtt-home-assistant subscribe to startup one topic for every partition in your risco alarm panel configuration.
+risco-mqtt-home-assistant subscribes at startup one topic for every partition in your risco alarm panel configuration.
 
-Format of the topics are `riscopanel/alarm/${partition.id}/set` where partition.id is the id of the partition
+Topics format is `riscopanel/alarm/<partition_id>/set` where **partition_id** is the id of the partition
 
-Payload could be : **disarmed** if risco panel is disarmed,**armed_home** if risco panel is **armed_home** and armed_away if risco panel is armed_away.
+Payload could be : **disarmed** if risco panel is in disarmed mode,**armed_home** if risco panel is in armed at home mode and **armed_away** if risco panel is in armed away mode.
 
 ## Publish Topics
 
-risco-mqtt-home-assistant publish one topic for every partition and for every zones in your risco alarm panel configuration.
+risco-mqtt-home-assistant publishes one topic for every partition and for every zones in your risco alarm panel configuration.
 
-Format of partitions topics are `riscopanel/alarm/${partition.id}/status` where **partition.id** is the id of the partition
+Partitions topics format is `riscopanel/alarm/<partition_id>/status` where **partition_id** is the id of the partition
 
-Payload could be : **disarmed** if risco panel is disarmed,**armed_home** if risco panel is **armed_home** and armed_away if risco panel is armed_away.
+Payload could be : **disarmed** if risco panel is in disarmed mode,**armed_home** if risco panel is in armed at home mode and **armed_away** if risco panel is in armed away mode.
 
-Format of zones topics are `riscopanel/alarm/${partition.id}/sensor/${zone.zoneID}/status` where **partition.id** is the id of the partition and **zone.zoneID** is the id of the zone.
+Zones topics format is `riscopanel/alarm/<partition_id>/sensor/<zone_id>/status` where **partition_id** is the id of the partition and **zone_id** is the id of the zone.
 
 Payload could be : **triggered** if zone is curently triggered, and **idle** if zone is currently idle.
 
-In addition for every zones, risco-mqtt-home-assistant publish a topic for every zone with the payload of all the infos of the zone in json format. Format of these topics are `riscopanel/alarm/${partition.id}/sensor/${zone.zoneID}` where **partition.id** is the id of the partition and **zone.zoneID** is the id of the zone.
+In addition for every zones, risco-mqtt-home-assistant publishes a topic for every zone with all the infos of the zone in the paylaod in json format. Topics format is `riscopanel/alarm/<partition_id>/sensor/<zone_id>` where **partition_id** is the id of the partition and **zone_id** is the id of the zone.
 
+## Home Assistant Auto Discovery
+
+risco-mqtt-home-assistant supports  feature [mqtt auto discovery](https://www.home-assistant.io/docs/mqtt/discovery/) feature.
+
+Default `<discovery_prefix>` is **homeassistant**. You can change it overwriting in **home-assistant-discovery-prefix** config.
 
 ## Usage
 
 This is the simplest usage you can do with risco-mqtt-home-assistant
 
-### Start risco-mqtt client
+### Start risco-mqtt-home-assistant client
 
 ```
-const riscoMqttHassio = require('../risco-mqtt-home-assistant')
+const riscoMqttHassio = require('risco-mqtt-home-assistant')
 const config = require('./config.json')
 
 riscoMqttHassio(config)
--- risco-mqtt-home-assistant.js
+++ risco-mqtt-home-assistant.js
@@ -1,13 +1,23 @@
 const mqtt = require('mqtt')
 const nodeRiscoClient = require('node-risco-client')
 
-const INTERVAL_POLLING = 5000
 const ALARM_TOPIC = "riscopanel/alarm"
 const ALARM_TOPIC_REGEX = /^riscopanel\/alarm\/([0-9])*\/set$/m
+const RISCO_NODE_ID = 'risco-alarm-panel'
 
 module.exports = (config) => {
+    let {
+        username,
+        password,
+        pin,
+        "language-id": languageId,
+        "mqtt-url": mqttURL,
+        "mqtt-username": mqttUsername,
+        "mqtt-password": mqttPassword,
+        "home-assistant-discovery-prefix": HASSIO_DISCOVERY_PREFIX_TOPIC = 'homeassistant',
+        "interval-polling": INTERVAL_POLLING = 5000
+    } = config
 
-    let { username, password, pin, languageId, mqttURL, mqttUsername, mqttPassword } = config
     if (!username) throw new Error('username options is required')
     if (!password) throw new Error('password options is required')
     if (!pin) throw new Error('pin options is required')
@@ -44,43 +54,75 @@ module.exports = (config) => {
         return alarmAction[code].call(this, partitionId)
     }
 
-    const retrieveAlarmStatus = () => {
-        riscoClient.getPartitions().then(partitions => {
-            for (const partition of partitions) {
-                let state = partition.armedState
-                mqttClient.publish(`${ALARM_TOPIC}/${partition.id}/status`, alarmPayload[state])
-                console.log(`published alarm status ${alarmPayload[state]} on partition ${partition.id}`)
-            }
-        }).catch(err => {
-            console.log(`error during get partitions after polling risco alarm panel`)
-            console.log(err)
-        })
+    const subscribeAlarmStateChange = (partitions) => {
+        for (const partition of partitions) {
+            console.log(`subscribe on ${ALARM_TOPIC}/${partition.id}/set topic`)
+            mqttClient.subscribe(`${ALARM_TOPIC}/${partition.id}/set`)
+        }
+        setInterval(retrieveAlarmStatus, INTERVAL_POLLING);
+    }
 
-        riscoClient.getZones().then(zones => {
-            for (const zone of zones) {
-                const partitionId = zone.part - 1
-                mqttClient.publish(`${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}`, JSON.stringify(zone))                
-                mqttClient.publish(`${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}/status`, sensorPayload[zone.status])                
+    const publishAlarmStateChange = (partitions) => {
+        for (const partition of partitions) {
+            let state = partition.armedState
+            mqttClient.publish(`${ALARM_TOPIC}/${partition.id}/status`, alarmPayload[state])
+            console.log(`published alarm status ${alarmPayload[state]} on partition ${partition.id}`)
+        }
+    }
+
+    const publishSensorsStateChange = (zones) => {
+        for (const zone of zones) {
+            const partitionId = zone.part - 1
+            mqttClient.publish(`${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}`, JSON.stringify(zone))
+            mqttClient.publish(`${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}/status`, sensorPayload[zone.status])
+        }
+    }
+
+    const autoDiscovery = (partitions, zones) => {
+        for (const partition of partitions) {
+            const payload = {
+                'name': `risco-alarm-panel-${partition.id}`,
+                'state_topic': `${ALARM_TOPIC}/${partition.id}/status`,
+                'command_topic': `${ALARM_TOPIC}/${partition.id}/set`
             }
-            console.log(`published ${zones.length} zones infos`)
+            mqttClient.publish(`${HASSIO_DISCOVERY_PREFIX_TOPIC}/alarm_control_panel/${RISCO_NODE_ID}/${partition.id}/config`, JSON.stringify(payload))
+            console.log(`published alarm_control_panel for homeassistant autodiscovery on partition ${partition.id}`)
+        }
+
+        for (const zone of zones) {
+            const partitionId = zone.part - 1
+            const nodeId = zone.zoneName.replace(' ', '-')
+            const payload = {
+                'name': `${zone.zoneName}`,
+                'payload_on': 'triggered',
+                'payload_off': 'idle',
+                'state_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}/status`,
+                'json_attributes_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}`
+            }            
+            mqttClient.publish(`${HASSIO_DISCOVERY_PREFIX_TOPIC}/binary_sensor/${nodeId}/${zone.zoneID}/config`, JSON.stringify(payload))
+        }
+        console.log(`published ${zones.length} binary_sensor for homeassistant autodiscovery`)
+    }
+
+    const retrieveAlarmStatus = () => {
+        Promise.all([riscoClient.getPartitions(), riscoClient.getZones()]).then(([partitions, zones]) => {
+            publishAlarmStateChange(partitions)
+            publishSensorsStateChange(zones)
         }).catch(err => {
-            console.log(`error during get zones after polling risco alarm panel`)
+            console.log(`error during retrieve status fo partitions and zones`)
             console.log(err)
         })
     }
 
     mqttClient.on('connect', () => {
         console.log(`connected on mqtt server: ${mqttURL}`)
-        riscoClient.getPartitions().then(partitions => {
-            if (partitions.length <= 0) mqttClient.subscribe(`${ALARM_TOPIC}/0/set`)
-            for (const partition of partitions) {
-                console.log(`subscribe on ${ALARM_TOPIC}/${partition.id}/set topic`)
-                mqttClient.subscribe(`${ALARM_TOPIC}/${partition.id}/set`)
-            }
-            setInterval(retrieveAlarmStatus, INTERVAL_POLLING);
+        Promise.all([riscoClient.getPartitions(), riscoClient.getZones()]).then(([partitions, zones]) => {
+            subscribeAlarmStateChange(partitions)
+            autoDiscovery(partitions, zones)
         }).catch(err => {
-            console.log(`error during get partitions after connected on mqtt server`)
+            console.log(`error during get partitions and zones on connect`)
             console.log(err)
+            process.exit(1)
         })
     })
 
