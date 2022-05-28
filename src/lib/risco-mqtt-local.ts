7efea517ffe871cb7bdabb079412f32132199b89
Mark Roberts <mark@markroberts.org.uk>
2022-05-28 22:09:50 +0200

    .

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -283,8 +283,8 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     }
   }
 
-  function outputStatus(output: Output) {
-    if (output.Status === "Deactivated") {
+  function outputStatus(outputId: number) {
+    if (panel.outputs.byId(outputId).Status === "Deactivated") {
       return '0';
     } else {
       return '1';
@@ -335,7 +335,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     return zones.values.filter(z => !z.NotUsed);
   }
   function activeOutputs(outputs: OutputList): Output[] {
-    return outputs.values.filter (o => o.Type !== null);
+    return outputs.values.filter (o => o.Label !== null);
   }
 
   function publishOnline() {
