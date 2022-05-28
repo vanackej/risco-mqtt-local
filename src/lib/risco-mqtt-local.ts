9e271dced993fb4b507ca78bc71a467f5b7734e9
Mark Roberts <mark@markroberts.org.uk>
2022-05-28 16:23:49 +0200

    .

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -259,7 +259,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
   }
 
   async function changeOutputPayload(activate: string, outputId: number) {
-    if (panel.outputs(outputId) === "Deactivated") {
+    if (panel.outputs.byId(outputId) === "Deactivated") {
       return '0';
     } else {
       return '1';
