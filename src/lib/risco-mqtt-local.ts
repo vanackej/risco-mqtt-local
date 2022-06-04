f7f2f88a1e90443dea5b1d6f71c69ebf2da32dc9
Mark Roberts <mark@markroberts.org.uk>
2022-06-04 07:19:46 +0100

    Update private output filtering

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -364,7 +364,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     return outputs.values.filter (o => o.UserUsable !== false);
   }
   function activePrivateOutputs(privateoutputs: OutputList): Output[] {
-    return privateoutputs.values.filter (o => o.UserUsable === false && o.Label !=='' && (o.Type === 1 || 3));
+    return privateoutputs.values.filter (o => o.UserUsable === false && o.Label !=='' && (o.Type === 1 || o.Type === 3));
   }
 
   function publishOnline() {
