7f48cd06d193f4d0db469d6740b06029fca93f66
Mark Roberts <mark@markroberts.org.uk>
2022-06-05 15:27:12 +0100

    .

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -309,7 +309,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
   }
 
   function outputState(output: Output) {
-    if (output.Status === 'Activated') {
+    if (output.Status.Activated === 'Activated') {
       return '1';
     } else if (output.Status === 'Pulsed') {
       return '1';
@@ -348,7 +348,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     mqttClient.publish(`${config.mqtt_alarm_topic}/alarm/output/${output.Id}/status`, outputStatus, {
       qos: 1, retain: false,
     });
-    logger.verbose(`[Panel => MQTT] Published output status ${outputStatus} on output ${output.Label}`);
+    logger.verbose(`[Panel => MQTT] Published output status ${output.Status} on output ${output.Label}`);
   }
 
   function publishZoneBypassStateChange(zone: Zone) {
